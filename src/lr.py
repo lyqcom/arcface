998422e63f99dd2e25eb96945457e4c05fb957c9
lyqcom <106475755+lyqcom@users.noreply.github.com>
2022-12-21 11:34:40 +0800

    Add files via upload

---
+++ src/lr.py
@@ -0,0 +1,28 @@
+
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""learning rate generator"""
+import numpy as np
+from mindspore import Tensor
+
+
+def lr_generator(lr_init, total_epochs, steps_per_epoch):
+    lr_each_step = []
+    for i in range(total_epochs):
+        if i in [10, 16, 21]:
+            lr_init *= 0.1
+        for _ in range(steps_per_epoch):
+            lr_each_step.append(lr_init)
+    lr_each_step = np.array(lr_each_step).astype(np.float32)
+    return Tensor(lr_each_step)
